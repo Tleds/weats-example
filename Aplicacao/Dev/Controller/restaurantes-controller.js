3c02837a8bd5b46bf6d60b55719b9b92769b74b4
Tiago <1085341@sga.pucminas.br>
2019-11-30 17:26:03 -0300

    alteracao bug

--- Aplicacao/Dev/Controller/restaurantes-controller.js
+++ Aplicacao/Dev/Controller/restaurantes-controller.js
@@ -4,6 +4,15 @@ const services = require('../Model/Services/restaurantes-services');
 const validate = require('./functions/validate-functions');
 
 module.exports = {
+    async getShopping(req, res, next){
+        if (req.userAccess == 0 || req.userAccess == 10) {
+            repository_restaurantes.all(req.headers.local).then(restaurante => {
+                res.json({ "id_mesa":req.headers.id_mesa, restaurante: restaurante }); //retorna o json com os usuários
+            });
+        } else {
+            res.status(403).json({ "auth": false, "message": "Acesso negado" });
+        }
+    },
     async  getId(req, res, next){
         if (req.userAccess != 0 || req.userAccess != 10) {res.status(403).json({ "auth": false, "message": "Acesso negado" });return}
         let resposta = await repository_restaurantes.all(req.headers.local);
@@ -13,19 +22,19 @@ module.exports = {
         return
     },
     async  post(req, res, next){
-        /*let restaurante = req.body;
+        let restaurante = req.body;
         if (validate.verificaNuloRestaurantes(restaurante) && (restaurante.senha != "" && typeof restaurante.senha != 'undefined')) {
-            services.validaEmailRestaurante(req.body).then(email{
+            services.validaEmailRestaurante(req.body).then(email => {
                 if (email > 0) {
                     res.status(500).json({ "message": "E-mail já cadastrado" });
                 }
                 if (email == 0) {
-                    services.validaCnpjRestaurante(req.body).then(cnpj{
+                    services.validaCnpjRestaurante(req.body).then(cnpj =>{
                         if (cnpj == 0) {
-                            services.create(req.body).then(result{
+                            services.create(req.body).then(result =>{
                                     res.status(200).json(result);
                                 })
-                                .catch(error{
+                                .catch(error => {
                                     res.status(404).json(error);
                                 })
     
@@ -37,7 +46,7 @@ module.exports = {
             });
         } else {
             res.status(400).json({ "message": "Erro : O atributo não pode ser nulo" });
-        }*/
+        }
     },
     async  put(req, res, next){ //request, responde e next
         /*let restaurante = req.body;
