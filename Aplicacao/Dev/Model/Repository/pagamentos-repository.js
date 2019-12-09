0b8e58203b4a95f000ebb00a45a21da5c1331686
Tiago <1085341@sga.pucminas.br>
2019-12-09 20:45:55 -0300

    alteracao repository de pagamentos

--- Aplicacao/Dev/Model/Repository/pagamentos-repository.js
+++ Aplicacao/Dev/Model/Repository/pagamentos-repository.js
@@ -1,57 +1,54 @@
 const pagamentos = require('../Entities/Pagamentos');
-const repository_usuarios = require('./usuarios-repository');
-const repository_restaurantes = require('./restaurantes-repository');
-const repository_mesas = require('./mesas-repository');
-const repository_pedidos = require('./pedidos-repository');
-const repository_forma_pagamento = require('./formas_pagamento-repository');
 
-exports.all = function() {
-    let reposta = pagamentos.findAll({ raw: true })
-    .catch(e=>{
-        return {"message":e,"result":false};
-    });
-    if(!resposta){return }
-    return {"message":resposta, "result":true};
-};
+module.exports = {
 
-exports.create = function Salvar(pagamento) {
-    return pagamentos.create({
-        id_forma_pagamento: pagamento.id_forma_pagamento,
-        id_usuario: pagamento.id_usuario,
-        id_restaurante: pagamento.id_restaurante,
-        id_mesa: pagamento.id_mesa,
-        id_pedido: pagamento.id_pedido,
-        preco_final: pagamento.preco_final
-    })
-}
-exports.read = function verificaEmail(pagamento) {
-    return pagamentos.findAll({ where: { id_usuario: pgamanto.id_usuario }, raw: true });
-}
-exports.update = function Atualizar(req) {
-    var pagamento = req.body
-    return pagamentos.findOne({
-        where: {
-            id: req.params.id
-        },
-        raw: true
-    }).then(id => {
-        if (typeof id != "undefined") {
-            pagamentos.update({
-                id_forma_pagamento: pagamento.id_forma_pagamento,
-                id_usuario: pagamento.id_usuario,
-                id_restaurante: pagamento.id_restaurante,
-                id_mesa: pagamento.id_mesa,
-                id_pedido: pagamento.id_pedido,
-                preco_final: pagamento.preco_final
-            }, {
-                where: { id: id.id }
-            })
-        }
-    });
-
-}
-exports.delete = function Deletar(req) {
-    return pagamentos.destroy({
-        where: { id: req.params.ident }
-    });
+    async all() {
+        let reposta = pagamentos.findAll({ raw: true })
+        .catch(e=>{
+            return {"message":e,"result":false};
+        });
+        if(!resposta){return }
+        return {"message":resposta, "result":true};
+    },
+    async create(pagamento) {
+        return pagamentos.create({
+            id_forma_pagamento: pagamento.id_forma_pagamento,
+            id_usuario: pagamento.id_usuario,
+            id_restaurante: pagamento.id_restaurante,
+            id_mesa: pagamento.id_mesa,
+            id_pedido: pagamento.id_pedido,
+            preco_final: pagamento.preco_final
+        })
+    },
+    async read(pagamento) {
+        return pagamentos.findAll({ where: { id_usuario: pgamanto.id_usuario }, raw: true });
+    },
+    async update(req) {
+        var pagamento = req.body
+        return pagamentos.findOne({
+            where: {
+                id: req.params.id
+            },
+            raw: true
+        }).then(id => {
+            if (typeof id != "undefined") {
+                pagamentos.update({
+                    id_forma_pagamento: pagamento.id_forma_pagamento,
+                    id_usuario: pagamento.id_usuario,
+                    id_restaurante: pagamento.id_restaurante,
+                    id_mesa: pagamento.id_mesa,
+                    id_pedido: pagamento.id_pedido,
+                    preco_final: pagamento.preco_final
+                }, {
+                    where: { id: id.id }
+                })
+            }
+        });
+    
+    },
+    async delete(req) {
+        return pagamentos.destroy({
+            where: { id: req.params.ident }
+        });
+    }
 }
