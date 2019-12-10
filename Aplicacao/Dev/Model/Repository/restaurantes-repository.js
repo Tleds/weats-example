1a7b2a81280b815f3614027f863c6e7dfefc3fd9
Tiago <1085341@sga.pucminas.br>
2019-12-10 16:30:09 -0300

    alteracao local 2

--- Aplicacao/Dev/Model/Repository/restaurantes-repository.js
+++ Aplicacao/Dev/Model/Repository/restaurantes-repository.js
@@ -2,7 +2,7 @@ const restaurantes = require('../Entities/Restaurantes');
 const bc = require('bcrypt');
 module.exports = {
     async all(local) {
-        let resposta =  await restaurantes.findAll({attributes: ['id', 'nome', 'email', 'cnpj', 'telefone', 'celular', 'avaliacao','imagem_restaurante'],where:{local : local}, raw: true })
+        let resposta =  await restaurantes.findAll({attributes: ['id', 'nome', 'email', 'cnpj', 'telefone', 'celular', 'avaliacao','imagem_restaurante'],where:{id_local : local}, raw: true })
         .catch(e=>{
             return {"message":e,"result":false}
         });
