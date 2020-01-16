06b96f83a451a9a3bd48cbdf16e39e3b6f1813f8
Tiago <1085341@sga.pucminas.br>
2020-01-16 10:36:19 -0300

    restante

---
+++ Aplicacao/Dev/Model/database/index.js
@@ -0,0 +1,41 @@
+'use strict';
+const Sequelize = require('sequelize');
+const config = require('../database/config/database');
+
+const Usuarios = require('./models/Usuarios');
+const Enderecos = require('./models/Enderecos');
+const Formas_pagamento = require('./models/Formas_pagamento');
+const Locals = require('./models/Locals');
+const Menus = require('./models/Menus');
+const Mesas = require('./models/Mesas');
+const Pagamentos = require('./models/Pagamentos');
+const Pedidos = require('./models/Pedidos');
+const Restaurantes = require('./models/Restaurantes');
+const Status = require('./models/Status');
+const avaliacao_restaurantes = require('./models/avaliacao_restaurante');
+const avaliacao_produtos = require('./models/avaliacao_produtos');
+const promocoes = require('./models/Promocoes');
+
+const connection = new Sequelize(config);
+
+Usuarios.init(connection);
+Enderecos.init(connection);
+Formas_pagamento.init(connection);
+Locals.init(connection);
+Menus.init(connection);
+Mesas.init(connection);
+Pagamentos.init(connection);
+Pedidos.init(connection);
+Restaurantes.init(connection);
+Status.init(connection); 
+avaliacao_restaurantes.init(connection);
+avaliacao_produtos.init(connection);
+promocoes.init(connection);
+
+Enderecos.associate(connection.models);
+Restaurantes.associate(connection.models);
+avaliacao_restaurantes.associate(connection.models);
+avaliacao_produtos.associate(connection.models);
+promocoes.associate(connection.models);
+
+module.exports = connection;
