a3d400110eb2ac30bea8c8322ef343c5d85fdcc4
Tiago <1085341@sga.pucminas.br>
2020-01-16 16:05:37 -0300

    alteracao cpf usuario

--- Aplicacao/Dev/Controller/usuarios-controller.js
+++ Aplicacao/Dev/Controller/usuarios-controller.js
@@ -47,7 +47,9 @@ module.exports= {
         
         if (!validate.verificaNuloUsuario(usuario)) {res.status(500).json({ "message": "Erro : O atributo não pode ser nulo" }); return}
         
-        if (services_usuarios.validarcpf(usuario.cpf)) {res.status(400).json({ "message": "CPF inválido" }); return}
+        
+        if (await validate.verificaCpf(usuario.cpf)) {res.status(400).json({ "message": "CPF inválido" }); return}
+        // if (services_usuarios.validarcpf(usuario.cpf)) {res.status(400).json({ "message": "CPF inválido" }); return}
         
         let alt = await services_usuarios.update(req);
         if(!alt.result){res.status(500).json(alt); return}
