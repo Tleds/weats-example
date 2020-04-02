ed430a150cab16d13ae0b2956ed81969dfc5a9dd
Tiago <1085341@sga.pucminas.br>
2020-04-02 15:48:30 -0300

    CT Session user

---
+++ Aplicacao/Dev/src/Controller/session_user-controller.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const Users = require('../Model/database/models/Users');
+
+class SessionUserController {
+  async createSession(req, res, next) {
+    const { email, password } = req.body;
+    const response = await Users.findOne({
+      where: { email },
+      attributes: ['id', 'id_access', 'password_hash'],
+    }).catch((e) => {
+      return res.status(500).json({ message: e });
+    });
+    if (!response) {
+      return res.status(400).json({ message: 'Incorrect email' });
+    }
+    if (!(await response.checkPassword(password))) {
+      return res.status(400).json({ message: 'Incorrect password' });
+    }
+    return res
+      .status(200)
+      .json({ message: 'Authenticated user', token: response.generateToken() });
+  }
+}
+
+module.exports = new SessionUserController();
